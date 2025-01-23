bcb45b74828c1705dec60251947a99660353dc03
pavander <pavander@umich.edu>
2025-01-23 14:08:46 -0500

    commit

--- createTables.sql
+++ createTables.sql
@@ -1,12 +1,11 @@
 CREATE TABLE Users(
-    user_id INTEGER NOT NULL,
+    user_id INTEGER PRIMARY KEY NOT NULL,
     first_name VARCHAR2(100) NOT NULL,
     last_name VARCHAR2(100) NOT NULL,
     year_of_birth INTEGER,
     month_of_birth INTEGER,
     day_of_birth INTEGER,
-    gender VARCHAR2(100),
-    PRIMARY KEY (user_id)
+    gender VARCHAR2(100)
 );
 
 CREATE TABLE Friends(
@@ -17,11 +16,10 @@ CREATE TABLE Friends(
 );
 
 CREATE TABLE Cities(
-    city_id INTEGER NOT NULL,
+    city_id INTEGER PRIMARY KEY NOT NULL,
     city_name VARCHAR2(100) NOT NULL,
     state_name VARCHAR2(100) NOT NULL,
     country_name VARCHAR2(100) NOT NULL,
-    PRIMARY KEY (city_id)
 );
 
 CREATE TABLE User_Current_Cities(
@@ -39,29 +37,44 @@ CREATE TABLE User_Hometown_Cities(
 );
 
 CREATE TABLE Messages(
-    message_id INTEGER NOT NULL,
+    message_id INTEGER PRIMARY KEY NOT NULL,
     sender_id INTEGER NOT NULL,
     receiver_id INTEGER NOT NULL,
     message_content VARCHAR2(2000) NOT NULL,
     sent_time TIMESTAMP NOT NULL,
-    PRIMARY KEY (message_id),
     FOREIGN KEY (sender_id) REFERENCES Users(user_id),
     FOREIGN KEY (receiver_id) REFERENCES Users(user_id)
 );
 
 CREATE TABLE Programs(
-    program_id INTEGER NOT NULL,
+    program_id INTEGER PRIMARY KEY NOT NULL,
     institution VARCHAR2(100) NOT NULL,
     concentration VARCHAR2(100) NOT NULL,
     degree VARCHAR2(100) NOT NULL,
-    PRIMARY KEY (program_id),
 );
 
 CREATE TABLE Education(
     user_id INTEGER NOT NULL,
-    program_id INTEGER NOT NULL,
+    program_id INTEGER PRIMARY KEY NOT NULL,
     program_year INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (program_id) REFERENCES Programs(program_id)
-)
+);
+
+CREATE TABLE User_Events(
+    event_id INTEGER PRIMARY KEY NOT NULL,
+    event_creator_id INTEGER NOT NULL,
+    event_name VARCHAR2(100) NOT NULL,
+    event_tagline VARCHAR2(100),
+    event_description VARCHAR2(100),
+    event_host VARCHAR2(100),
+    event_type VARCHAR2(100),
+    event_subtype VARCHAR2(100),
+    event_address VARCHAR2(2000),
+    event_city_id INTEGER NOT NULL,
+    event_start_time TIMESTAMP,
+    event_end_time TIMESTAMP,
+    FOREIGN KEY (event_creator_id) REFERENCES Users(user_id),
+    FOREIGN KEY (event_city_id) REFERENCES Cities(city_id)
+);
 
