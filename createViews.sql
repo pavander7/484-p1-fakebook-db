aec61daf048238f558cccb5ac9b8acee495b29a3
Paul Vander Woude <pavander@umich.edu>
2025-01-30 15:12:28 -0500

    debugged multiple errors

--- createViews.sql
+++ createViews.sql
@@ -1,4 +1,4 @@
-PRAGMA foreign_keys = ON;
+-- PRAGMA foreign_keys = ON;
 
 CREATE VIEW View_User_Information 
     AS SELECT Users.*,
@@ -14,9 +14,9 @@ CREATE VIEW View_User_Information
               Programs.degree AS program_degree
         FROM Users
         LEFT JOIN User_Current_Cities ON Users.user_id = User_Current_Cities.user_id
-        LEFT JOIN Cities AS Current_Cities ON User_Current_Cities.city_id = Current_Cities.city_id
+        LEFT JOIN Cities Current_Cities ON User_Current_Cities.current_city_id = Current_Cities.city_id
         LEFT JOIN User_Hometown_Cities ON Users.user_id = User_Hometown_Cities.user_id
-        LEFT JOIN Cities AS Hometown_Cities ON User_Hometown_Cities.city_id = Hometown_Cities.city_id
+        LEFT JOIN Cities Hometown_Cities ON User_Hometown_Cities.hometown_city_id = Hometown_Cities.city_id
         LEFT JOIN Education ON Users.user_id = Education.user_id
         LEFT JOIN Programs ON Education.program_id = Programs.program_id
 ;
@@ -27,10 +27,15 @@ CREATE VIEW View_Are_Friends
 ;
 
 CREATE VIEW View_Photo_Information
-    AS SELECT Albums.*,
-              Photos.*,
-        FROM Albums
-        LEFT JOIN Photos ON Albums.album_id = Photos.album_id
+    AS SELECT a.album_id, 
+              a.album_owner_id AS owner_id, 
+              a.cover_photo_id, 
+              a.album_name, a.album_created_time, a.album_modified_time, 
+              a.album_link, a.album_visibility, 
+              p.photo_id, p.photo_caption,
+              p.photo_created_time, p.photo_modified_time, p.photo_link
+        FROM Albums a
+        LEFT JOIN Photos p ON a.album_id = p.album_id
 ;
 
 CREATE VIEW View_Event_Information
@@ -53,6 +58,6 @@ CREATE VIEW View_Event_Information
 ;
 
 CREATE VIEW View_Tag_Information
-    AS SELECT Tags.*
+    AS SELECT *
     FROM Tags
 ;
